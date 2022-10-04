47308c57f2eb6109bddd54b04c216aae38ffe2df
vdemyankov <vdemyankov@example.ru>
2022-10-04 12:59:03 +0300

    added files terraform

---
+++ modules/instance/modules_instance_variables.tf
@@ -0,0 +1,14 @@
+variable "instance_family_image" {
+  description = "Instance image"
+  type        = string
+  default     = "lamp"
+}
+
+variable "vpc_subnet_id" {
+  description = "VPC subnet network id"
+  type        = string
+}
+variable "vpc_subnet_zone" {
+  description = "VPC zone id"
+  type        = string
+}
